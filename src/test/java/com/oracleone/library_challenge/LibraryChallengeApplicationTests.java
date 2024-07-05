0c5bd8f5c9be61044d83b9420088faccc03e13e1
Angela Rojas <angela.rojas22@gmail.com>
2024-07-04 23:06:51 -0400

    Creating the project

---
+++ src/test/java/com/oracleone/library_challenge/LibraryChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oracleone.library_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
